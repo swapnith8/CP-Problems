94743ce6d84b62055b56c87aaffce6e4a1e43a48
swapnith8 <kswapnith@msitprogram.net>
2020-07-14 22:53:55 +0530

    Jul 14  22:53:55

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -6,4 +6,11 @@
 
 def fun_getinrange(x, bound1, bound2):
 	# your code goes here
-	return -1
+	if (bound1 > x < bound2):
+		return x
+	elif(x>bound2):
+		return bound2
+	else:		
+		return bound1
+
+	
