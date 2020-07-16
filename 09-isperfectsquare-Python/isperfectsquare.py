457afb422170e98fd8a91da156a2e6421fbfda1a
swapnith8 <kswapnith@msitprogram.net>
2020-07-16 18:22:32 +0530

    Jul 16  18:22:32

--- 09-isperfectsquare-Python/isperfectsquare.py
+++ 09-isperfectsquare-Python/isperfectsquare.py
@@ -6,7 +6,8 @@ import math
 
 def isperfectsquare(n):
 	# your code goes here
-	if(type(temp)==int and temp>0):
+
+	if(type(n)==int and n>0):
 		return int(math.sqrt((math.pow(n,2)))) == n
 	return False
 
