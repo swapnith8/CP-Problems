3b8c1d39893fb40dfac030f8d4cd61836a668eba
swapnith8 <kswapnith@msitprogram.net>
2020-07-21 17:48:00 +0530

    done

--- 08-isrotated-Python/isrotated.py
+++ 08-isrotated-Python/isrotated.py
@@ -6,9 +6,13 @@
 
 def isrotated(str1, str2):
 	#Your code goes here
-	str2 = str2[len(str2)::-1]
-	if(str1 == str2 or sorted(str1) == sorted (str2)):
-		return True
+	
+	if len(str1)!= len(str2) or str1 ==str2:
+		return False
 	else:
-		return False		
+		temp =str1+str1
+		if str2 in temp :
+			return True
+		else:
+			return False		
 	
