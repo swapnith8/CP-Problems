476cb743d59f325fba3d7d166d85b03d4cf9bbbc
swapnith8 <kswapnith@msitprogram.net>
2020-07-24 12:00:15 +0530

    Jul 24  12:00:15

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -3,5 +3,18 @@
 
 
 def fun_hasnoprimes(l):
+	for i in range(len(l)):
+		for j in range(len(l[i])):
+			x = l[i][j]
+			y = 1
+			c =0
+			while(x>y):
+				if(x%y==0):
+					c +=1
+					y+=1
+				else:
+					y+=1
+			if c<=1:
+				return False			
 	return True
 
