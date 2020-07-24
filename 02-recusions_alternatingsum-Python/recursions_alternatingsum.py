bbe687879117c0c27542b4d6cadef69bbc1909a0
swapnith8 <kswapnith@msitprogram.net>
2020-07-24 14:22:06 +0530

    Jul 24  14:22:06

--- 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
+++ 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
@@ -7,7 +7,7 @@
 def fun_recursions_alternatingsum(l):
 	if(len(l)==0):
 		 return 0
-	if(ln(l)==1):
+	if(len(l)==1):
 		return l[0]
 	if(len(l)>1):
 		return l[0]-l[1]+fun_recursions_alternatingsum(l[2:])
