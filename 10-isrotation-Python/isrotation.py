b6d411effa0c696142a89fecf74b560cffcb9116
swapnith8 <kswapnith@msitprogram.net>
2020-07-24 16:47:34 +0530

    Jul 24  16:47:34

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -7,4 +7,12 @@
 
 def isrotation(x, y):
 	# Your code goes here
-	pass
+	x = str(x)
+	y = srt(y)
+	if (len(x)!=len(y)):
+		return False
+	if y in x*2	:
+		return True
+	if x == ''.join (reversed(y)):
+		return True
+	return False		
