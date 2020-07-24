97f1187845e2267b9ea4694accaa313dd0ead67b
swapnith8 <kswapnith@msitprogram.net>
2020-07-24 16:33:37 +0530

    Jul 24  16:33:37

--- 11-findzerowithbisection-Python/findzerowithbisection.py
+++ 11-findzerowithbisection-Python/findzerowithbisection.py
@@ -3,9 +3,16 @@
 # Rather than exhaustively trying things starting at 1, suppose instead we pick a number in the middle of this range
 # Bisection search works when value of function varies monotonically with input
 # If g, the users input/guess, is less than/greater than the midpoint of the range, then that number becomes the new high point of said range and is then factored into the new search.
-
+import math
 def findzerowithbisection(x, epsilon):
 	# epsilon and step are initialized
 	# don't change these values
 	# epsilon
 	# your code starts here
+	mid = (1+x)/2
+	if (findzerowithbisection(1,epsilon)*findzerowithbisection(mid,epsilon)<0):
+		mid1 =(1+mid)/2
+		findzerowithbisection(mid1,epsilon)
+	if(findzerowithbisection(mid,epsilon)*findzerowithbisection(x,epsilon)<0):
+		mid1 =(mid+x)/2
+		findzerowithbisection(mid1,epsilon)
