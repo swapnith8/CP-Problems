a797252fba7f61ef95c0ed0185d9f16f4bb22560
swapnith8 <kswapnith@msitprogram.net>
2020-07-24 14:24:12 +0530

    Jul 24  14:24:12

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -8,5 +8,8 @@
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
+
+
 def fun_recursion_onlyevendigits(l): 
+
 		return []
